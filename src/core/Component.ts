fee228a5475e5a435befe37b06bb6f196fcff7ea
Seoin <seoin2269@naver.com>
2024-12-01 19:44:37 +0900

    state, setState, render class로 구현, 모듈화

---
+++ src/core/Component.ts
@@ -0,0 +1,26 @@
+interface IState {
+  [key: string]: any;
+}
+
+export default class Component {
+  $target: HTMLElement;
+  state: IState;
+
+  constructor($target: HTMLElement) {
+    this.$target = $target;
+    this.state = {};
+    this.setup();
+    this.render();
+  }
+  setup() { };
+  template() { return ''; }
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+  setEvent() { }
+  setState(newState: IState) {
+    this.state = { ...this.state, newState };
+    this.render()
+  }
+}
